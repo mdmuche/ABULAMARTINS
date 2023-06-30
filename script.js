9500693af6429b97602a454b56ad0d82f04d4552
abula martins <abulamartins@gmail.com>
2023-06-30 19:20:15 +0100

    added error and others

---
+++ script.js
@@ -0,0 +1,44 @@
+// header toggle
+
+let menuBtn = document.getElementById("menuBtn");
+
+menuBtn.addEventListener("click", function (e) {
+  document.querySelector("body").classList.toggle("mobile-nav-active");
+  this.classList.toggle("fa-xmark");
+});
+
+//typing effect
+let typed = new Typed(".auto-input", {
+  strings: ["Front-end Developer", "Freelancer!", "Web Developer"],
+  typedSpeed: 100,
+  backSpeed: 100,
+  backDelay: 2000,
+  loop: true,
+});
+
+//active link state on scroll
+
+//get all links
+let navLinks = document.querySelectorAll("nav ul li a");
+//get all sections
+let sections = document.querySelectorAll("section");
+console.log(sections);
+
+window.addEventListener("scroll", function () {
+  const scrollPos = window.scrollY + 20;
+  sections.forEach((section) => {
+    if (
+      scrollPos > section.offsetTop &&
+      scrollPos < section.offsetTop + section.offsetHeight
+    ) {
+      navLinks.forEach((link) => {
+        link.classList.remove("active");
+        if (
+          section.getAttribute("id") === link.getAttribute("href").substring(1)
+        ) {
+          link.classList.add("active");
+        }
+      });
+    }
+  });
+});
